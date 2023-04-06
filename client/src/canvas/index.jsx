06260d2250636f65f1d2966acb6c284f6b393f85
taixinF <yclzbb1349458861@163.com>
2023-04-06 21:56:13 +0800

    add backDrop

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
-import { Canvas } from '@react-three/fiber'
-import { Center, Environment } from "@react-three/drei";
+import {Canvas} from '@react-three/fiber'
+import {Center, Environment} from "@react-three/drei";
 
 import Shirt from './Shirt'
 import CameraRig from './CameraRig'
+import Backdrop from "./Backdrop";
+
 const CanvasModule = () => {
 
     return (
         <Canvas>
-            <ambientLight intensity={0.5} />
-            <Environment preset="city" />
-            
+            <ambientLight intensity={0.5}/>
+            <Environment preset="city"/>
+
             <CameraRig>
+                <Backdrop/>
                 <Center>
-                    <Shirt />
+                    <Shirt/>
                 </Center>
             </CameraRig>
         </Canvas>
