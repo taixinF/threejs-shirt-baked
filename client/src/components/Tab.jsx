709711679ef433adc121b66f862b50eff604767f
taixinF <1349458861@qq.com>
2023-04-07 17:58:43 +0800

    add

--- client/src/components/Tab.jsx
+++ client/src/components/Tab.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import state from "../store/index.js";
+import {useSnapshot} from "valtio";
 
-const Tab = () => {
+const Tab = ({tab, isActiveTab, isFilterTab, handleClick}) => {
+    const snap = useSnapshot(state)
+    const activeStyles = isFilterTab && isFilterTab ? {
+        background: snap.color,
+        opacity: 0.5
+    } : {background: 'transparent', opacity: 1}
+
     return (
-        <div>Tab</div>
+        <div
+            className={`tab-btn ${isFilterTab ? 'rounded-full glass-morphism' : 'rounded-4'}`}
+            onClick={handleClick}
+        >
+            <img src={tab.icon} alt={tab.name}/>
+        </div>
     )
 }
 
